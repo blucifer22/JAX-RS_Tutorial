8f8bc413b48eb097e52b84f3e9a4181887deaa77
Marc Chmielewski <waterdragon2145@gmail.com>
2019-12-30 17:09:50 -0500

    JavaDoc

--- src/main/java/org/example/resources/MessageResource.java
+++ src/main/java/org/example/resources/MessageResource.java
@@ -8,6 +8,14 @@ import javax.ws.rs.core.MediaType;
 @Path("/messages") //Annotation that adds this class to the /messages of the URL path
 public class MessageResource {
 
+    /**
+     * This method acts as a HTTP GET method (resource handler) for a
+     * basic "Hello World" resource.
+     * It is annotated with @GET to indicate this to Jersey
+     * Additionally, it is annotated with @Produces to so that the
+     * servlet knows it will be receiving plain text.
+     * @return A plaintext "Hello world!" String
+     */
     @GET //Maps the getter method to the GET HTTP method!
     @Produces(MediaType.TEXT_PLAIN) //Indicates that the response is plain text
     public String getMessages()
