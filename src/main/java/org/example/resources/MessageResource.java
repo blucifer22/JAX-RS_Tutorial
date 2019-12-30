a3579f5287cfe2b456b46621b22357d9f2942362
Marc Chmielewski <waterdragon2145@gmail.com>
2019-12-30 17:02:58 -0500

    Implemented MessageResource

--- src/main/java/org/example/resources/MessageResource.java
+++ src/main/java/org/example/resources/MessageResource.java
@@ -1,4 +1,17 @@
 package org.example.resources;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/messages") //Annotation that adds this class to the /messages of the URL path
 public class MessageResource {
+
+    @GET //Maps the getter method to the GET HTTP method!
+    @Produces(MediaType.TEXT_PLAIN) //Indicates that the response is plain text
+    public String getMessages()
+    {
+        return "Hello world!";
+    }
 }
