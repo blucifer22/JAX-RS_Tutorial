c0879372105856192d867bb828f6f7b509dc1a82
Marc Chmielewski <waterdragon2145@gmail.com>
2019-12-30 21:56:47 -0500

    Added DatabaseClass and MessageService CRUD

--- src/main/java/org/example/service/MessageService.java
+++ src/main/java/org/example/service/MessageService.java
@@ -1,19 +1,44 @@
 package org.example.service;
 
+import org.example.database.DatabaseClass;
 import org.example.model.Message;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MessageService {
 
+    //Since this is static future requests point to the same instance (until server is reset)
+    private Map<Long, Message> messages = DatabaseClass.getMessages();
+
     public List<Message> getAllMessages()
     {
-        Message m1 = new Message(1L, "Hello world!", "Marc");
-        Message m2 = new Message(2L, "Hello Jersey!", "Marc");
-        List<Message> list = new ArrayList<>();
-        list.add(m1);
-        list.add(m2);
-        return list;
+        return new ArrayList<>(messages.values());
+    }
+
+    public Message getMessage(long id)
+    {
+        return messages.get(id);
+    }
+
+    public Message addMessage(Message message)
+    {
+        message.setId(messages.size() + 1);
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message updateMessage(Message message)
+    {
+        if(message.getId() <= 0)
+            return null;
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message removeMessage(long id)
+    {
+        return messages.remove(id);
     }
 }
