df790bcfdacb6daafc831d082056abed680157d8
Marc Chmielewski <waterdragon2145@gmail.com>
2019-12-31 12:41:23 -0500

    Cleaning tree

--- src/main/java/org/example/resources/MessageResource.java
+++ src/main/java/org/example/resources/MessageResource.java
@@ -5,11 +5,12 @@ import org.example.service.MessageService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Path("/messages") //Annotation that adds this class to the /messages of the URL path
+@Path("/messages") //Annotation that adds this class to the /messages of the URL path (Top-Level annotation)
 public class MessageResource {
 
     MessageService messageService = new MessageService();
@@ -28,4 +29,15 @@ public class MessageResource {
     {
         return messageService.getAllMessages();
     }
+
+    @GET
+    @Path("/{messageId}") //Map to subsequent path /messages/{messageId}
+    @Produces(MediaType.APPLICATION_XML)
+    //Uses the PathParam feature to grab the parameter from the URL
+    //Furthermore, this utilizes Jersey's inbuilt autoboxing and unboxing for PathParams
+    public Message getMessage(@PathParam("messageId") long messageId)
+    {
+        return messageService.getMessage(messageId);
+    }
+
 }
