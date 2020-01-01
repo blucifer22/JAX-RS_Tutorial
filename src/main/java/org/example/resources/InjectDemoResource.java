50e1acbc7f8a80b84466948376b3d6355f8f1088
Marc Chmielewski <waterdragon2145@gmail.com>
2020-01-01 14:11:09 -0500

    JavaDoc

--- src/main/java/org/example/resources/InjectDemoResource.java
+++ src/main/java/org/example/resources/InjectDemoResource.java
@@ -9,6 +9,14 @@ import javax.ws.rs.core.MediaType;
 @Consumes(MediaType.TEXT_PLAIN)
 public class InjectDemoResource {
 
+    /**
+     * This is a sample HTTP GET method to demonstrate some different types of injection
+     * params. All of the params are extracted using JAX-RS annotations as shown below
+     * @param matrixParam This variable holds a MatrixParam formatted URL component (;key=value)
+     * @param header This variable holds a Param extracted from the HTTP header sent with the GET request
+     * @param cookie This variable holds a Param extracted from a cookie sent with the GET request
+     * @return a labeled plaintext representation of the different parameters.
+     */
     @GET
     @Path("/annotations")
     public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam,
