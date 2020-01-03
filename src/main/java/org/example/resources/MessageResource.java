bd65cefc251575f1e242a51ac31782f5c2838c5f
Marc Chmielewski <waterdragon2145@gmail.com>
2020-01-02 21:01:32 -0500

    Implemented content negotiation

--- src/main/java/org/example/resources/MessageResource.java
+++ src/main/java/org/example/resources/MessageResource.java
@@ -14,6 +14,8 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 @Path("/messages") //Annotation that adds this class to the /messages of the URL path (Top-Level annotation)
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
     MessageService messageService = new MessageService();
@@ -27,9 +29,26 @@ public class MessageResource {
      * @return An XML representation of all the messages on the server
      */
     @GET //Maps the getter method to the GET HTTP method!
-    @Produces(MediaType.APPLICATION_JSON) //Indicates that the response is JSON
-    public List<Message> getMessages(@BeanParam MessageFilterBean filterBean)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Message> getJsonMessages(@BeanParam MessageFilterBean filterBean)
     {
+        System.out.println("JSON method called");
+        if(filterBean.getYear() > 0)
+        {
+            return messageService.getAllMessagesForYear(filterBean.getYear());
+        }
+        if(filterBean.getStart() >= 0 && filterBean.getSize() > 0)
+        {
+            return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
+        }
+        return messageService.getAllMessages();
+    }
+
+    @GET //Maps the getter method to the GET HTTP method!
+    @Produces(MediaType.TEXT_XML)
+    public List<Message> getXmlMessages(@BeanParam MessageFilterBean filterBean)
+    {
+        System.out.println("XML method called");
         if(filterBean.getYear() > 0)
         {
             return messageService.getAllMessagesForYear(filterBean.getYear());
@@ -43,8 +62,6 @@ public class MessageResource {
 
     @PUT
     @Path("/{messageId}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Message updateMessage(@PathParam("messageId") long id, Message message)
     {
         message.setId(id);
@@ -53,7 +70,6 @@ public class MessageResource {
 
     @DELETE
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public void deleteMessage(@PathParam("messageId") long id)
     {
         messageService.removeMessage(id);
@@ -61,8 +77,6 @@ public class MessageResource {
 
 
     @POST //Map the method to the HTTP POST method
-    @Consumes(MediaType.APPLICATION_JSON) //This annotation tells Jersey that it will be accepting JSON input
-    @Produces(MediaType.APPLICATION_JSON) //This annotation tells Jersey that it will be producing JSON output
     public Response addMessage(Message message, @Context UriInfo uriInfo)
     {
         Message newMessage = messageService.addMessage(message);
@@ -78,7 +92,6 @@ public class MessageResource {
 
     @GET
     @Path("/{messageId}") //Map to subsequent path /messages/{messageId}
-    @Produces(MediaType.APPLICATION_JSON)
     //Uses the PathParam feature to grab the parameter from the URL
     //Furthermore, this utilizes Jersey's inbuilt autoboxing and unboxing for PathParams
     public Message getMessage(@PathParam("messageId") long messageId, @Context UriInfo uriInfo)
